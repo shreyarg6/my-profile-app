e09d433ae507d534e7a95f48690e92b59ab4514c
Shreya Guddeti <shreyarg6@gmail.com>
2025-01-21 15:59:38 -0500

    com

---
+++ src/components/card1.jsx
@@ -0,0 +1,24 @@
+import img from "../assets"
+const card1 = () => {
+    const name = "jon"
+    const title = "SE"
+    const email = "jon.com"
+
+    return(
+        <div className = "profile-card">
+            <div className = "profiel-card__image">
+                <img src={img} alt="profile-pic"/>
+            </div>
+
+            <div className = "profiel-card__content">
+                <p>{name}</p>
+                <p>{title}</p>
+                <p>{email}</p>
+                <p><a href={'mailto:${email}'}>{email}</a></p>
+            </div>
+
+        </div>
+
+    );
+}
+export default card1;
