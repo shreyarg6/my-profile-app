4deae5cc743a67ea18af39f54774939ffaa6e58d
Shreya Guddeti <shreyarg6@gmail.com>
2025-01-20 23:57:51 -0500

    com

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Vite and React logos to learn mores
       </p>
     </>
   )
